bd0ef8d578c32aade16bf5cd09aaf7d5ca6cd59a
Fidel Como <comofidelluis@gmail.com>
2023-06-21 18:52:54 +0000

    primeiro commit: definição das funções em items.h e algumas funções uteis em auxiliares.h

---
+++ auxiliares.h
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int iPayload;
+    struct Node* ptrLeft;
+    struct Node* ptrRight;
+};
+
+struct Node* newNode(int iData)
+{
+    struct Node* newNodePtr = (struct Node*) malloc(sizeof(struct Node));
+    newNodePtr->iPayload = iData;
+    newNodePtr->ptrLeft = nullptr;
+    newNodePtr->ptrRight = nullptr;
+
+    return newNodePtr;
+}
+
+struct Node* insertNode(struct Node* root, int iData)
+{
+    if (root == nullptr) return newNode(iData);
+  
+    if (iData < root->iPayload) root->ptrLeft = insertNode(root->ptrLeft, iData); 
+    else root->ptrRight = insertNode(root->ptrRight, iData);
+
+    return root;
+}
+
+void traverseInOrder(struct Node* ptrStartingNode)
+{
+    if (ptrStartingNode == nullptr) return;
+
+    traverseInOrder(ptrStartingNode->ptrLeft);
+    cout << " " << ptrStartingNode->iPayload;
+    traverseInOrder(ptrStartingNode->ptrRight);
+}
+
+struct Node* lesserLeaf(struct Node* node)
+{
+    struct Node* ptrCurrent = node;
+
+    while(ptrCurrent && ptrCurrent->ptrLeft != nullptr) ptrCurrent = ptrCurrent->ptrLeft;
+
+    return ptrCurrent;
+}
+
+void swapNodes(Node* &node1, Node* &node2)
+{
+    Node* temp = node1->ptrRight;
+    node1->ptrRight = node2->ptrRight;
+    if (temp != node2) node2->ptrRight = temp;
+    else node2->ptrRight = node1;
+    
+    temp = node1->ptrLeft;
+    node1->ptrLeft = node2->ptrLeft;
+    if (temp != node2) node2->ptrLeft = temp;
+    else node2->ptrLeft = node1;
+    
+    temp = node1;
+    node1 = node2;
+    node2 = temp;
+    
+    temp = node1->ptrRight;
+    while (temp->ptrLeft != node1) temp = temp->ptrLeft;
+    temp->ptrLeft = node2;
+    
+    return;
+}
+
+struct Node* deleteNode(struct Node* root, int iData)
+{
+    if (root == nullptr) return root; 
+
+    if (iData < root->iPayload) root->ptrLeft = deleteNode(root->ptrLeft, iData);
+    else if (iData > root->iPayload) root->ptrRight = deleteNode(root->ptrRight, iData);
+
+    else
+    {  
+        struct Node* temp = nullptr;
+
+        if (root->ptrLeft == nullptr)
+        {
+            temp = root->ptrRight;
+            free(root);
+            
+            return temp;
+        }
+        else if (root->ptrRight == nullptr)
+        {
+            temp = root->ptrLeft;
+            free(root);
+            
+            return temp;
+        }
+
+        temp = lesserLeaf(root->ptrRight);
+
+        swapNodes(root, temp);
+        
+        root->ptrRight = deleteNode(root->ptrRight, temp->iPayload);
+    }
+    return root;
+}
+
+struct Node* searchNode(struct Node* node, int iData)
+{
+    if (node == nullptr) return nullptr;
+
+    else if (node->iPayload == iData) return node;
+
+    else if (iData < node->iPayload) return searchNode(node->ptrLeft, iData);
+    return searchNode(node->ptrRight, iData);
+}
---
+++ items.h
@@ -0,0 +1,57 @@
+#include "auxiliares.h"
+
+Node* constroi_txt()
+{
+  return nullptr;
+}
+
+Node* constroi_input()
+{
+  return nullptr;
+}
+
+int altura(Node* ptrHead)
+{
+  return 0;
+}
+
+int tamanho(Node* ptrHead)
+{
+  return 0;
+}
+
+Node* inserir(Node* ptrHead)
+{
+  return nullptr;
+}
+
+Node* remover(Node* ptrHead)
+{
+  return nullptr;
+}
+
+Node* buscar(Node* ptrHead)
+{
+  return nullptr;
+}
+
+bool completa(Node* ptrHead)
+{
+  return 0;
+}
+
+bool prefeita(Node* ptrHead)
+{
+  return 0;
+}
+
+void exebe_BFS(Node* ptrHead)
+{
+  
+}
+
+Node* ordenar_Bubble()
+{
+  return nullptr;
+}
+
